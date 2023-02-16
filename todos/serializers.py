a1ef51f654f696c33e33e6f77ed3e80cbdaf5061
Katie Pestotnik <katie.pestotnik@gmail.com>
2023-02-15 21:57:21 -0700

    todo for render deployment

---
+++ todos/serializers.py
@@ -0,0 +1,8 @@
+from .models import Todo
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id', 'subject', 'details']
